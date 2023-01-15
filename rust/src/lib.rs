a26af9a5876d61e53598b663ddd04e03481971b0
James Moore <james@restphone.com>
2023-01-15 11:15:45 -0800

    Work in progress

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -14,6 +14,6 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, nom-sample!");
+pub fn rust_code_goes_here() -> String {
+    "Rust!".into()
 }
--- ts/src/use_rust.ts
+++ ts/src/use_rust.ts
@@ -1,3 +1,5 @@
-export function do_rust_thing() {
-    return "expected value"
+import * as RustCode from "../../rust/pkg/banshee_nom_sample"
+
+export function do_rust_thing(): string {
+    return RustCode.rust_code_goes_here()
 }
