9efd1564c0fcfbfa3a9b5b560024c453614054b4
James Moore <james@restphone.com>
2023-01-17 15:56:36 -0800

    work in progress

---
+++ ts/src/plain.js
@@ -0,0 +1,3 @@
+// const RustCode = require("rust-webpack-template")
+import * as RustCode from "rust-webpack-template"
+console.log(RustCode.rust_code_goes_here_v2())
--- ts/src/use_rust.ts
+++ ts/src/use_rust.ts
@@ -1,5 +1,6 @@
-import * as RustCode from "../../rust/pkg/banshee_nom_sample"
+import * as RustCode from "rust-webpack-template"
 
 export function do_rust_thing(): string {
+    // return "not rust"
     return RustCode.rust_code_goes_here()
 }
