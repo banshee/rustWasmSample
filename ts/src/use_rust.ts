697e983dd1768a11f31d27ddb5079340f43fed3a
James Moore <james@restphone.com>
2023-01-18 14:41:30 -0800

    mess around with package.json

--- ts/src/use_rust.ts
+++
@@ -1,5 +0,0 @@
-import * as RustCode from "@banshee.com-nodejs/rust-nom-sample"
-
-export function do_rust_thing(): string {
-    return RustCode.this_is_a_rust_function()
-}
--- ts/test/test_rust.ts
+++ ts/test/test_rust.ts
@@ -4,7 +4,7 @@ import * as RustCode from "@banshee.com-nodejs/rust-nom-sample"
 
 describe("Using rust code", () => {
     describe("basic functionality", async () => {
-        it("should call a function", async () => {
+        it("can call a Rust function", async () => {
             expect(RustCode.this_is_a_rust_function()).to.eq("This string is generated in Rust")
         })
     })
